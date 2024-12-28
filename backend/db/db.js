3bb93b4716fcd01ef0d30fe708bceb7d9a410988
Sadanand J M <147255461+Mjsadanand@users.noreply.github.com>
2024-12-28 09:19:33 +0530

    grid resolved

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,10 +1,9 @@
 import mongoose from 'mongoose';
 import { initializeAdmin } from '../util/initAdmin.js';
 
-const password = encodeURIComponent("Veda@718"); // Encodes special characters in the password
-const MONGODB_URL = process.env.MONGO_URI || `mongodb+srv://sadanandjm:${password}@cluster0.y3hoa.mongodb.net/fileManager`;
-
-// const MONGODB_URL = process.env.MONGO_URI || 'mongodb+srv://sadanandjm:Veda%40718@cluster0.y3hoa.mongodb.net/fileManager';
+const username = encodeURIComponent('sadanandjm');  // Use encodeURIComponent to encode special characters
+const password = encodeURIComponent('Veda@718');  // Encode special characters in password
+const MONGODB_URL = process.env.MONGO_URI || `mongodb+srv://${username}:${password}@cluster0.y3hoa.mongodb.net/fileManager`;
 
 const connectDB = async () => {
   try {
@@ -16,5 +15,5 @@ const connectDB = async () => {
     process.exit(1); // Exit the process with failure
   }
 };
- 
+
 export default connectDB;
