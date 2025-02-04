bb3a35c44d757317a83295dc5fbac20e4e59e2d5
Sadanand J M <147255461+Mjsadanand@users.noreply.github.com>
2025-02-04 17:39:47 +0530

    Squid games

--- frontend/src/components/Files.jsx
+++ frontend/src/components/Files.jsx
@@ -60,7 +60,7 @@ function Files({ subject, setSelectedSubject }) {
 
   return (
     <div className="files-container">
-      <h2 className="files-header">Notes for {subject.name}</h2>
+      <h3 className="files-header">Notes for {subject.name}</h3>
       <button className="btn" onClick={() => setSelectedSubject(null)}>
         <svg height="16" width="16" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1024 1024"><path d="M874.690416 495.52477c0 11.2973-9.168824 20.466124-20.466124 20.466124l-604.773963 0 188.083679 188.083679c7.992021 7.992021 7.992021 20.947078 0 28.939099-4.001127 3.990894-9.240455 5.996574-14.46955 5.996574-5.239328 0-10.478655-1.995447-14.479783-5.996574l-223.00912-223.00912c-3.837398-3.837398-5.996574-9.046027-5.996574-14.46955 0-5.433756 2.159176-10.632151 5.996574-14.46955l223.019353-223.029586c7.992021-7.992021 20.957311-7.992021 28.949332 0 7.992021 8.002254 7.992021 20.957311 0 28.949332l-188.073446 188.073446 604.753497 0C865.521592 475.058646 874.690416 484.217237 874.690416 495.52477z"></path></svg>
         Back
@@ -105,10 +105,19 @@ function Files({ subject, setSelectedSubject }) {
         </button>
       </div>
       {isLoading && (
-        <div className="loader-popup">
-          <div className="loader"></div>
-          <p>Uploading...</p>
-        </div>
+        <><div className="loader">
+          <svg viewBox="0 0 80 80">
+            <circle r="32" cy="40" cx="40" id="test"></circle>
+          </svg>
+        </div><div className="loader triangle">
+            <svg viewBox="0 0 86 80">
+              <polygon points="43 8 79 72 7 72"></polygon>
+            </svg>
+          </div><div className="loader">
+            <svg viewBox="0 0 80 80">
+              <rect height="64" width="64" y="8" x="8"></rect>
+            </svg>
+          </div></>
       )}
       <div className="notifications-container">
         <div className="info">
@@ -136,7 +145,7 @@ function Files({ subject, setSelectedSubject }) {
             </div>
             <div className="alert-prompt-wrap">
               <p className="text-sm text-yellow-700">
-                If your files are not in the PDF format, you can convert them into PDF format then upload.
+                If your files are not in the PDF format, you can convert them into PDF format and then upload.
                 <a className="alert-prompt-link" href="https://www.ilovepdf.com/" target="_blank">Let convert</a>
               </p>
             </div>
