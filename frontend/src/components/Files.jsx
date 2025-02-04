1e3e7c17063dd31be8ce8bf405dc3f559d2414e7
Sadanand J M <147255461+Mjsadanand@users.noreply.github.com>
2025-02-04 19:33:53 +0530

    adding Class

--- frontend/src/components/Files.jsx
+++ frontend/src/components/Files.jsx
@@ -156,7 +156,7 @@ function Files({ subject, setSelectedSubject }) {
 
       </div>
       {isLoading && (
-        <><div className="loader">
+        <div className="loader-container"><div className="loader">
           <svg viewBox="0 0 80 80">
             <circle r="32" cy="40" cx="40" id="test"></circle>
           </svg>
@@ -168,7 +168,7 @@ function Files({ subject, setSelectedSubject }) {
             <svg viewBox="0 0 80 80">
               <rect height="64" width="64" y="8" x="8"></rect>
             </svg>
-          </div></>
+          </div></div>
       )}
       <div className="notifications-container">
         <div className="info">
