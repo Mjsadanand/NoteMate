42743f54ab58cda3bcbfa195b208e0f07b02afd2
Sadanand J M <147255461+Mjsadanand@users.noreply.github.com>
2025-02-05 21:12:27 +0530

    Back button added

--- frontend/src/components/Files.jsx
+++ frontend/src/components/Files.jsx
@@ -59,41 +59,15 @@ function Files({ subject, setSelectedSubject }) {
 
   return (
     <div className="files-container">
+      <button onClick={() => setSelectedSubject(null)} className="but">
+        <svg height="16" width="16" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1024 1024"><path d="M874.690416 495.52477c0 11.2973-9.168824 20.466124-20.466124 20.466124l-604.773963 0 188.083679 188.083679c7.992021 7.992021 7.992021 20.947078 0 28.939099-4.001127 3.990894-9.240455 5.996574-14.46955 5.996574-5.239328 0-10.478655-1.995447-14.479783-5.996574l-223.00912-223.00912c-3.837398-3.837398-5.996574-9.046027-5.996574-14.46955 0-5.433756 2.159176-10.632151 5.996574-14.46955l223.019353-223.029586c7.992021-7.992021 20.957311-7.992021 28.949332 0 7.992021 8.002254 7.992021 20.957311 0 28.949332l-188.073446 188.073446 604.753497 0C865.521592 475.058646 874.690416 484.217237 874.690416 495.52477z"></path></svg>
+        <span>Back</span>
+      </button>
       <h3 className="files-header">Notes for {subject.name}</h3>
       {/* <button className="btn" onClick={() => setSelectedSubject(null)}>
         <svg height="16" width="16" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1024 1024"><path d="M874.690416 495.52477c0 11.2973-9.168824 20.466124-20.466124 20.466124l-604.773963 0 188.083679 188.083679c7.992021 7.992021 7.992021 20.947078 0 28.939099-4.001127 3.990894-9.240455 5.996574-14.46955 5.996574-5.239328 0-10.478655-1.995447-14.479783-5.996574l-223.00912-223.00912c-3.837398-3.837398-5.996574-9.046027-5.996574-14.46955 0-5.433756 2.159176-10.632151 5.996574-14.46955l223.019353-223.029586c7.992021-7.992021 20.957311-7.992021 28.949332 0 7.992021 8.002254 7.992021 20.957311 0 28.949332l-188.073446 188.073446 604.753497 0C865.521592 475.058646 874.690416 484.217237 874.690416 495.52477z"></path></svg>
         Back
       </button> */}
-      <div className="styled-wrapper">
-        <button className="bn" onClick={() => setSelectedSubject(null)}>
-          <div className="button-box">
-            <span className="button-elem">
-              <svg
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                className="arrow-icon"
-              >
-                <path
-                  fill="black"
-                  d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"
-                ></path>
-              </svg>
-            </span>
-            <span className="button-elem">
-              <svg
-                fill="black"
-                viewBox="0 0  24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                className="arrow-icon"
-              >
-                <path
-                  d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"
-                ></path>
-              </svg>
-            </span>
-          </div>
-        </button>
-      </div>
 
       <div className="files-list">
         {files.length > 0 ? (
@@ -112,7 +86,7 @@ function Files({ subject, setSelectedSubject }) {
               >
                 {file.name}
               </a> <br />
-              <button style={{backgroundColor: "transparent",color: "black"}}
+              <button style={{ backgroundColor: "transparent", color: "black" }}
                 className="buttonDownload"
                 onClick={() => window.open(file.link, "_blank")}
               >
@@ -182,7 +156,7 @@ function Files({ subject, setSelectedSubject }) {
                 Maximum file size is 10MB. Supported file types is only PDF.
               </p>
             </div>
-          </div> 
+          </div>
         </div>
       </div>
       <div className="notifications-container">
